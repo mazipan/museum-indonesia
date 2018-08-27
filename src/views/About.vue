720dcc041ef19bde5c112139712860b44df40340
Irfan Maulana <irfan.maulana@bizzy.co.id>
2018-08-27 19:15:47 +0700

    dummy interaction

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,67 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <label for="province-selection">Province: </label>
+    <select id="province-selection" v-model="selectedProvince">
+      <option disabled value="">Please select one</option>
+      <option :value="province.kode_wilayah" v-for="province in provinceList" :key="province.kode_wilayah">
+        {{ province.nama }}
+      </option>
+    </select>
+
+    <div v-if="museumList && museumList.length > 0">
+      <table>
+        <thead>
+          <th>
+            <td>No</td>
+            <td>Name</td>
+          </th>
+        </thead>
+        <tbody>
+          <tr v-for="(museum, index) in museumList" :key="museum.museum_id">
+            <td>{{ index+1 }}</td>
+            <td>{{ museum.nama }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'map',
+  data () {
+    return {
+      selectedProvince: '',
+      provinceList: [],
+      museumList: []
+    }
+  },
+  watch: {
+    selectedProvince (newValue) {
+      console.log(newValue)
+      if (newValue) {
+        fetch(`/museum-indonesia/data/${newValue.trim()}.json`)
+          .then(resp => resp.json())
+          .then(data => {
+            console.log(data)
+            this.museumList = data.data
+          })
+      }
+    }
+  },
+  methods: {
+    fetchData () {
+      fetch('/museum-indonesia/data/province.json')
+        .then(resp => resp.json())
+        .then(data => {
+          console.log(data)
+          this.provinceList = data.data
+        })
+    }
+  },
+  mounted () {
+    this.fetchData()
+  }
+}
+</script>
