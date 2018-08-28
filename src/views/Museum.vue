f79646dc0b98b355b22580f43d20e95bc7bef379
Irfan Maulana <irfan.maulana@bizzy.co.id>
2018-08-28 12:06:56 +0700

    add map, marker and info window

--- src/views/Museum.vue
+++ src/views/Museum.vue
@@ -25,13 +25,17 @@
         </tbody>
       </table>
     </div>
+
+    <div id="mapbox__container" class="mapbox__container"></div>
   </div>
 </template>
 
 <script>
+const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js')
+
 export default {
   name: 'museum',
-  data () {
+  data() {
     return {
       selectedProvince: '',
       provinceList: [],
@@ -39,37 +43,68 @@ export default {
     }
   },
   watch: {
-    selectedProvince (newValue) {
-      console.log(newValue)
+    selectedProvince(newValue) {
       if (newValue) {
         fetch(`/museum-indonesia/data/${newValue.trim()}.json`)
           .then(resp => resp.json())
           .then(data => {
-            console.log(data)
             this.museumList = data.data
+            this.initMap(this.museumList)
           })
       }
     }
   },
   methods: {
-    fetchData () {
+    fetchData() {
       fetch('/museum-indonesia/data/province.json')
         .then(resp => resp.json())
         .then(data => {
-          console.log(data)
           this.provinceList = data.data
         })
+    },
+    generateMarker(map, museumList) {
+      museumList.map(item => {
+        const el = document.createElement('div')
+        el.className = 'marker'
+        el.style.backgroundImage = 'url(https://i.imgur.com/MK4NUzI.png)'
+        el.style.width = '32px'
+        el.style.height = '40px'
+
+        const popup = new mapboxgl.Popup({ offset: 25 }).setText(item.nama)
+
+        new mapboxgl.Marker(el)
+          .setLngLat([item.bujur, item.lintang])
+          .setPopup(popup) // sets a popup on this marker
+          .addTo(map)
+      })
+    },
+    initMap(museumList) {
+      mapboxgl.accessToken =
+        'pk.eyJ1IjoibWF6aXBhbiIsImEiOiJjamxjNXl0MWk0bXZuM3FsbXJ4Zno4dzBwIn0.5iwp2hDExpabMN6dIrkdeg'
+
+      const map = new mapboxgl.Map({
+        container: 'mapbox__container',
+        style: 'mapbox://styles/mapbox/streets-v10',
+        zoom: 4.0,
+        center: [120.0, 1.38994]
+      })
+
+      map.addControl(new mapboxgl.NavigationControl())
+      this.generateMarker(map, museumList)
     }
   },
-  mounted () {
+  mounted() {
     this.fetchData()
   }
 }
 </script>
 
 <style scoped>
-.museum{
+.museum {
   text-align: left;
 }
+.mapbox__container {
+  height: 300px;
+  width: 100%;
+}
 </style>
-
